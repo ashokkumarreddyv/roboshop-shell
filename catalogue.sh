d9d35e2f80b4fd9abcdb696c794d5a4149631605
ashokv <ashok3akr@gmail.com>
2025-02-19 18:50:01 +0530

    learn -1

--- catalogue.sh
+++ catalogue.sh
@@ -2,7 +2,10 @@ component=catalogue
 source common.sh
 nodejs_user
 
-artifact_download
+mkdir /app
+curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component-v3.zip
+cd /app
+unzip /tmp/$component.zip
 
 cd /app
 npm install
