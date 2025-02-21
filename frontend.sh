f0d1f07a54cfeece568a48ea32b1c36a1aff5a3c
ashokv <ashok3akr@gmail.com>
2025-02-21 20:09:42 +0530

    learn -1

--- frontend.sh
+++ frontend.sh
@@ -1,26 +1,26 @@
 
 print_head() {
   echo -e "\e[35m$*\e[0m"
-  echo "*********************" >> $log_file
+  echo "*********************" &>> $log_file
 }
 
 log_file=/tmp/roboshop.log
 
 
 print_head disable nginx
-dnf module disable nginx -y >> $log_file
-dnf module enable nginx:1.24 -y >> $log_file
-print_head install nginx >> $log_file
-dnf install nginx -y >> $log_file
+dnf module disable nginx -y &>> $log_file
+dnf module enable nginx:1.24 -y &>> $log_file
+print_head install nginx &>> $log_file
+dnf install nginx -y &>> $log_file
 
-cp nginx.conf /etc/nginx/nginx.conf >> $log_file
+cp nginx.conf /etc/nginx/nginx.conf &>> $log_file
 
 rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip >> $log_file
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>> $log_file
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip >> $log_file
+unzip /tmp/frontend.zip &>> $log_file
 
-print_head menable nginx >> $log_file
-systemctl enable nginx >> $log_file
-systemctl start nginx >> $log_file
-systemctl restart nginx >> $log_file
+print_head menable nginx &>> $log_file
+systemctl enable nginx &>> $log_file
+systemctl start nginx &>> $log_file
+systemctl restart nginx &>> $log_file
