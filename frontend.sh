835ca25b37c36cbce37c9d483408e7ec30841e6f
ashokv <ashok3akr@gmail.com>
2025-02-21 19:41:28 +0530

    learn -1

--- frontend.sh
+++ frontend.sh
@@ -3,12 +3,12 @@ print_head() {
   echo -e "\e[35m$*\e[0m"
 
 print_head disable nginx
-dnf module disable nginx -y
-dnf module enable nginx:1.24 -y
-print_head install nginx
-dnf install nginx -y
+dnf module disable nginx -y >/tmp/roboshop.log
+dnf module enable nginx:1.24 -y >/tmp/roboshop.log
+print_head install nginx >/tmp/roboshop.log
+dnf install nginx -y >/tmp/roboshop.log
 
-cp nginx.conf /etc/nginx/nginx.conf
+cp nginx.conf /etc/nginx/nginx.conf >/tmp/roboshop.log
 
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
