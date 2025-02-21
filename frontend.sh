de285e268897c790afe690509f26bd16ae5e937e
ashokv <ashok3akr@gmail.com>
2025-02-21 20:00:31 +0530

    learn -1

--- frontend.sh
+++ frontend.sh
@@ -2,7 +2,7 @@
 print_head() {
   echo -e "\e[35m$*\e[0m"
   echo "*********************" >>/$log_file
-
+}
 log_file=/tmp/roboshop.log
 
 
@@ -14,13 +14,12 @@ dnf install nginx -y >>/$log_file
 
 cp nginx.conf /etc/nginx/nginx.conf >>/$log_file >>/$log_file
 
-rm -rf /usr/share/nginx/html/* >>/$log_file
+rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip >>/$log_file
-cd /usr/share/nginx/html >>/$log_file
+cd /usr/share/nginx/html
 unzip /tmp/frontend.zip >>/$log_file
 
 print_head menable nginx >>/$log_file
 systemctl enable nginx >>/$log_file
 systemctl start nginx >>/$log_file
 systemctl restart nginx >>/$log_file
-}
