a1e64d0d90561302b91fbe91ac801491b42dd058
ashokv <ashok3akr@gmail.com>
2025-02-23 10:46:57 +0530

    learn -1

--- common.sh
+++ common.sh
@@ -36,10 +36,10 @@ nodejs_user() {
 
 exit_status() {
        if [ $1 -eq 0 ]; then
-             echo -e "\e[32m >>success\e[0m"
+        echo -e "\e[32m >>success\e[0m"
        else
-             echo -e "\e[31m >>failure\e[0m"
-             exit
+        echo -e "\e[31m >>failure\e[0m"
+        exit
        fi
 
 
