dd0d20853b6914d35aa50e592b0c3cec794c37a7
ashokv <ashok3akr@gmail.com>
2025-02-23 09:45:27 +0530

    learn -1

--- user.sh
+++ user.sh
@@ -1,5 +1,5 @@
 print_head(){
- echo -e "\e[34m$*\e[0m"
+ echo -e "\e[36m$*\e[0m"
 
 }
 
@@ -9,14 +9,18 @@ source common.sh
 
 print_head disable nodejs
 dnf module disable nodejs -y
+echo $?
 
 print_head enable nodejs
 dnf module enable nodejs:20 -y
+echo $?
 
 print_head install nodejs
 dnf install nodejs -y
+echo $?
 cp user.service /etc/systemd/system/user.service
 useradd roboshop
+echo $?
 
 artifact_download
 
@@ -24,6 +28,7 @@ cd /app
 
 print_head install npm
 npm install
+echo $?
 
 demon_setup
 echo $?
